0fb75dd514343a43bee01254ec5886923ed44a89
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-20 01:37:34 +0600

    added new file

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -2,19 +2,21 @@ const Movie = ({ movie }) => {
     const { title, director, image, rating, year, genre } = movie;
 
     return (
-        <div className="text-center p-4 gird sm:grid-cols-1 md:grid-cols-3">
+        <div>
             <div className="flex justify-center">
                 <img
-                    className="w-full max-w-xs h-auto object-contain rounded shadow-md"
+                    className="w-full  max-w-xs h-auto object-contain rounded shadow-md"
                     src={image}
                     alt={title}
                 />
             </div>
-            <h2 className="text-lg font-semibold mt-4">Name: {title}</h2>
-            <p>Director: {director}</p>
-            <p>Genre: {genre}</p>
-            <p>Year: {year}</p>
-            <p>Rating: {rating}</p>
+            <div className="text-center">
+                <h2 className="text-lg font-semibold mt-4 "> {title}</h2>
+                <p>Director: {director}</p>
+                <p>Genre: {genre}</p>
+                <p>Year: {year}</p>
+                <p>Rating: {rating}</p>
+            </div>
         </div>
     );
 };
